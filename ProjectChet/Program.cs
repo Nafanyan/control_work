d8c21fc9b7d573d54878d19a42d97512fb4aa204
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2021-12-27 20:47:07 +0300

    Написал решение поставленной задачи на C#

--- ProjectChet/Program.cs
+++ ProjectChet/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] RemoveZero (int[] sourceArray, int notZero)
+{
+    int[] resultArray = new int[notZero];
+    for (int i = 0; i < notZero; i++)
+    {
+        if (sourceArray[i] != 0)
+        {
+            resultArray[i] = sourceArray[i];
+        }
+    }
+    return resultArray;
+}
+
+int[] inputArray = {1, 2, 4, 12, 7, 34, 0, 6};
+int[] zeroInputArray = new int[inputArray.Length];
+int count = 0;
+PrintArray(inputArray);
+Console.Write(" => ");
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i] % 2 == 0)
+    {
+        zeroInputArray[count] = inputArray[i];
+        count++;
+    }
+}
+PrintArray(RemoveZero(zeroInputArray, count));
+
